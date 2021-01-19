7b3ab2f26a3cad8eb93bfbd813fa3e3811574009
dehmer <horst.dehmer@syncpoint.io>
2021-01-19 21:20:40 +0100

    colored signal beams.

--- ws-server.mjs
+++ ws-server.mjs
@@ -30,7 +30,12 @@ const randomPosition = (lat, lng) => {
 }
 
 // constant between server restarts:
-const beamUUID = 'dbe9a008-d5e6-4a31-91a9-d8909dc3fd5d'
+const beamUUIDs = [
+  'dbe9a008-d5e6-4a31-91a9-d8909dc3fd5d',
+  '5e67dfbd-dd5e-45c7-8ba5-458a6891c4c7',
+  'ed806e39-b7d4-4d49-a17c-5b2f268f5c22'
+]
+
 const jammerUUID = '276f23c6-700f-4676-9959-928286eeea97'
 const uaUUIDs = [
   'b67ad373-5783-45ef-9ea3-638c22285f83',
@@ -132,24 +137,33 @@ const signalBeam = function () {
   var active = true
 
   const loop = bearing => {
-    const tallinn = [24.753574, 59.436962] // [Longitude, Latitude]
-    const distance = 300 * 1000 // [m]
-    setTimeout(() => {
-      const point = new LatLon(tallinn[1], tallinn[0]).destinationPoint(distance, bearing)
-      const feature = {
-        type: 'Feature',
-        id: `feature:${layerId.split(':')[1]}/${beamUUID}`,
-        geometry: { type: "LineString", coordinates: [
-          tallinn,
-          [point.lng, point.lat]
-        ]},
-        properties: {
-          t: "Tallinn Lighthouse",
-          color: 'green'
-        }
-      }
+    const xs = [
+      ['Tallinn', [24.753574, 59.436962], 'cyan'], // [Longitude, Latitude]
+      ['Tartu', [26.728493, 58.378025], 'magenta'],
+      ['Pärnu', [24.49711, 58.38588], 'black']
+    ]
 
-      this.send(JSON.stringify(feature))
+    const distance = 200 * 1000 // [m]
+
+    setTimeout(() => {
+      const features = xs.map((x, index) => {
+        const point = new LatLon(x[1][1], x[1][0]).destinationPoint(distance, bearing * (index + 1))
+        return {
+          type: 'Feature',
+          id: `feature:${layerId.split(':')[1]}/${beamUUIDs[index]}`,
+          geometry: { type: "LineString", coordinates: [
+            x[1],
+            [point.lng, point.lat]
+          ]},
+          properties: {
+            t: `${x[0]} Lighthouse`,
+            color: x[2]
+          }
+        }
+      })
+
+      const collection = { type: 'FeatureCollection', features }
+      this.send(JSON.stringify(collection))
       active && loop((bearing + 1) % 360)
     }, 50)
   }
